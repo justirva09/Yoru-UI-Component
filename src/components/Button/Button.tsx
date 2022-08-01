1186c48af0f947185be30b26f0e5c61501bbeeae
Muhammad Irva <muhammad.irva@bibit.id>
2022-08-01 21:27:23 +0700

    initial commit & setup module bundler

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "secondary" | "success" | "danger" | "warning" | "info" | "light" | "dark";
+}
+const Button: React.FC<ButtonProps> = (props) => {
+  const { variant, children } = props;
+  return (
+    <button type="button" className={`yoru-button yoru-button--${variant}`} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
