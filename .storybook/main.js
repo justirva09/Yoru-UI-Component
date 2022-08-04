46c59b1136b4108977301ecc1b3bab253f59a341
Leonard <leonard.monosa@gambitstocks.com>
2022-08-04 22:44:20 +0700

    add storybook entrypoints

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,16 @@
+const path = require('path')
+
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../packages/**/stories/*.stories.tsx"
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react"
+  addons: [],
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@yoru-ui/react": path.resolve(__dirname, "../packages/react/src"),
+    }
+    config.resolve.extensions.push(".ts", ".tsx")
+    return config
+  },
 }
