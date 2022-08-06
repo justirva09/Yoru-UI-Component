e8871e7648c0f805c15c3615641281a661d122aa
Muhammad Irva <48151470+justirva09@users.noreply.github.com>
2022-08-06 18:30:14 +0700

    Setup/codebase (#4)
    * finished setup turbo & tsup build bundler
    
    * finished setup jest for unit test
    
    * setup eslint & husky pre-commit
    
    * test husky
    
    * test husky
    
    * test:husky pre-commit
    
    * test:husky pre-commit
    
    * test:husky pre-commit
    
    * test:husky pre-commit
    
    * testing husky
    
    * [feat/huksy] test husky
    
    * finished setup husky & eslint
    
    * test: husky
    
    * test husky
    
    * test husky
    
    * add eslint & husky
    
    Co-authored-by: Muhammad Irva <muhammad.irva@bibit.id>

---
+++ lint-staged.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
+  // '**/*.{js,jsx,ts,tsx}': 'pnpm clean',
+  '**/*.{js,jsx,ts,tsx,json,css,scss}': ['prettier --write'],
+  // '**/packages/**/*.{ts,tsx}': 'jest --bail --findRelatedTests',
+};
--- packages/Button/src/button.tsx
+++ packages/Button/src/button.tsx
@@ -1,15 +1,14 @@
-import React from "react";
+import React from 'react';
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "primary" | "secondary" | "success" | "danger" | "warning" | "info" | "light" | "dark";
+  variant?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark';
 }
-const Button: React.FC<ButtonProps> = (props) => {
-  const { variant = "primary", children } = props;
+
+export const Button: React.FC<ButtonProps> = props => {
+  const { variant = 'primary', children } = props;
   return (
     <button type="button" className={`yoru-button yoru-button--${variant}`} {...props}>
       {children}
     </button>
   );
 };
-
-export default Button;
